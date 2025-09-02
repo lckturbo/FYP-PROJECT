e34c7544d12c4050e52b93515677c0693457e8bb
claytxnzz <clayplayplay@gmail.com>
2025-09-02 16:06:27 +0800

    f

--- Competition/Assets/Script/Quest/QuestTester.cs
+++ Competition/Assets/Script/Quest/QuestTester.cs
@@ -18,7 +18,7 @@ public class QuestTestStarter : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.R) && runtime != null)
         {
-            runtime.AddItem("item1");
+            runtime.AddItem("Item1");
         }
     }
 }
