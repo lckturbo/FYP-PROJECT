200df499874c634c57aa743fc26ea677014b0f59
12-Jaslin <jaslinx2903@gmail.com>
2025-09-02 17:35:56 +0800

    BasicEnemies

--- Competition/Assets/Scripts/Jas/NormalEnemy.cs
+++ Competition/Assets/Scripts/Jas/NormalEnemy.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NormalEnemy : EnemyBase
+public class NormalEnemy : BasicFSM
 {
     protected override void Attack()
     {
@@ -13,30 +11,5 @@ public class NormalEnemy : EnemyBase
 
         // play animation
         // player take damage
-    }
-
-    protected override void StateMachine()
-    {
-        switch (_states)
-        {
-            case EnemyStates.Idle:
-                Idle();
-                break;
-            case EnemyStates.Patrol:
-                Patrol();
-                break;
-            case EnemyStates.Attack:
-                Attack();
-                break;
-            case EnemyStates.Investigate:
-                Investigate();
-                break;
-            case EnemyStates.Chase:
-                Chase();
-                break;
-            case EnemyStates.Death:
-                Death();
-                break;
-        }
     }
 }
