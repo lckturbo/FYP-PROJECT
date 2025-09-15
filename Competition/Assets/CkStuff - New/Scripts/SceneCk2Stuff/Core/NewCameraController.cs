40765120cf353278f315dd6c0518088187b0d94e
lckturbo <chengkel341@gmail.com>
2025-09-15 14:28:07 +0800

    small fixes

--- Competition/Assets/CkStuff - New/Scripts/SceneCk2Stuff/Core/NewCameraController.cs
+++ Competition/Assets/CkStuff - New/Scripts/SceneCk2Stuff/Core/NewCameraController.cs
@@ -1,25 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NewCameraController : MonoBehaviour
 {
     public Transform target;
-    public float smoothing;
+    public Vector3 offset = new Vector3(0, 0, -10);
+    [Range(0.02f, 0.5f)] public float smoothTime = 0.15f;
+    private Vector3 _vel;
 
-    // Start is called before the first frame update
-    void Start()
+    void LateUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if(transform.position != target.position)
-        {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
-            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
-        }
+        if (!target) return;
+        Vector3 desired = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, desired, ref _vel, smoothTime);
     }
 }
