66a9999ee23cf549bea21938d445bc7212843cec
12-Jaslin <jaslinx2903@gmail.com>
2025-09-22 17:07:02 +0800

    saveload done , havent test yet lol

--- Competition/Assets/JasStuff/Scripts/SaveLoad/SaveLoadSystem.cs
+++ Competition/Assets/JasStuff/Scripts/SaveLoad/SaveLoadSystem.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
 
 public class SaveLoadSystem : MonoBehaviour
 {
+    [Header("File Storage Config")]
+    [SerializeField] private string fileName;
+
     public static SaveLoadSystem instance;
     private GameData gameData;
+    private List<IDataPersistence> dataPersistenceObjs;
+    private FileDataHandler fileDataHandler;
+
     private void Awake()
     {
         if (!instance)
@@ -14,6 +22,8 @@ public class SaveLoadSystem : MonoBehaviour
 
     private void Start()
     {
+        this.fileDataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
+        this.dataPersistenceObjs = FindAllDataPersistenceObjects();
         LoadGame();
     }
 
@@ -23,21 +33,37 @@ public class SaveLoadSystem : MonoBehaviour
     }
     public void LoadGame()
     {
-        // TODO: load saved data from file using data handler
+        // load saved data from file using data handler
+        this.gameData = fileDataHandler.Load();
         // if no data, initialize new game
-        if (gameData == null) 
-            NewGame();
+        if (this.gameData == null) NewGame();
 
-        // TODO: push loaded data to all other scripts that need it
+        // push loaded data to all other scripts that need it
+        foreach(IDataPersistence dataObjs in dataPersistenceObjs)
+        {
+            dataObjs.LoadData(gameData);
+        }
     }
     public void SaveGame()
     {
-        // TODO: pass data to other scripts so they can update
-        // TODO: save data to a file using data handler
+        // pass data to other scripts so they can update
+        foreach (IDataPersistence dataObjs in dataPersistenceObjs)
+        {
+            dataObjs.SaveData(ref gameData);
+        }
+        // save data to a file using data handler
+        fileDataHandler.Save(gameData);
     }
 
     public void OnApplicationQuit()
     {
         SaveGame();
+    }
+
+    private List<IDataPersistence> FindAllDataPersistenceObjects()
+    {
+        IEnumerable<IDataPersistence> dataPersistenceObjs = FindObjectsOfType<MonoBehaviour>().OfType<IDataPersistence>();
+
+        return new List<IDataPersistence>(dataPersistenceObjs);
     }
 }
