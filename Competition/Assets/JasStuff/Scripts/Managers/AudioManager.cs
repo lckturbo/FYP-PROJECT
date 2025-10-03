7b2c618216d94f510e9ac93cb1ab1fb059d64fe1
12-Jaslin <jaslinx2903@gmail.com>
2025-10-03 14:57:00 +0800

    audiomanager done

---
+++ Competition/Assets/JasStuff/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [SerializeField] private Sound[] sounds;
+    private Dictionary<string, Sound> soundDictionary;
+
+    private void Awake()
+    {
+        if (!instance) instance = this;
+        else Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        soundDictionary = new Dictionary<string, Sound>();
+        
+        foreach(Sound sound in sounds)
+        {
+            if (sound.clip == null) return;
+
+            sound.audioSource = gameObject.AddComponent<AudioSource>();
+            sound.audioSource.clip = sound.clip;
+            sound.audioSource.volume = sound.volume;
+            sound.audioSource.pitch = sound.pitch;
+            sound.audioSource.loop = sound.loop;
+
+            soundDictionary[sound.clip.name] = sound;
+        }
+    }
+
+    public void PlaySound(string clipName)
+    {
+        if (soundDictionary.ContainsKey(clipName))
+            soundDictionary[clipName].audioSource.Play();
+    }
+    public void StopSound(string clipName)
+    {
+        if (soundDictionary.ContainsKey(clipName))
+            soundDictionary[clipName].audioSource.Stop();
+    }
+    public void StopAllSounds()
+    {
+        foreach (var sound in soundDictionary.Values)
+        {
+            if (sound.audioSource.isPlaying)
+                sound.audioSource.Stop();
+        }
+    }
+}
+
+[System.Serializable]
+public class Sound
+{
+    public AudioClip clip;
+    [Range(0, 1f)] public float volume = 1f;
+    [Range(0, 3f)] public float pitch = 1f;
+    public bool loop = false;
+
+    [HideInInspector] public AudioSource audioSource;
+}
