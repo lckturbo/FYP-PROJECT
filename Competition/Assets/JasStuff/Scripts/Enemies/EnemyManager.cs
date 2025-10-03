83724917f0f40a7e7f511b221d41b4c089fcf90a
12-Jaslin <jaslinx2903@gmail.com>
2025-10-03 10:22:45 +0800

    producer animation imported

---
+++ Competition/Assets/JasStuff/Scripts/Enemies/EnemyManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager instance;
+
+    private void Awake()
+    {
+        if (!instance) instance = this;
+        else Destroy(gameObject);
+    }
+
+
+}
